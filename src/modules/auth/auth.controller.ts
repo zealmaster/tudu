5a9073f4ae18efe5f0d838af43f1a84ed0faa269
zealmaster <onahkingsleyijuo@gmail.com>
2024-02-10 19:50:38 +0100

    created getCsrfToken endpoint, applied CsrfMiddleware to auth/csrf route  install cookie-parser

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-guard.guard';
 import { LoginUserDto } from './userLogin.dto';
+import { Request } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -14,5 +15,10 @@ export class AuthController {
       return req.user;
     }
     return await this.authService.loginUser(body.username);
+  }
+
+  @Get('csrf')
+  async getCsrfToken(@Req() req: Request) {
+    return {'csrf-token': req.csrfToken()};
   }
 }
